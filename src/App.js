d8049ee8f0b02e118b0cb3ac6cc4c75c1fc18355
DavidMilai <davidmilai67@gmail.com>
2022-06-08 15:52:05 +0300

    loading eth accounts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Web3 from 'web3';
 
 
 function App() {
 
-  const [name, setName] = useState('myName');
+  const [account, setAccount] = useState();
+
+
+  useEffect(() => {
+    async function loadAccounts(){
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+    }
+  },[])
+
 
   return (
     <div className="App">
       <header className="App-header">
-        Smart Ballot ({name})
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        Smart Ballot 
       </header>
+      <p>
+        your account: {account}
+      </p>
     </div>
   );
 }
