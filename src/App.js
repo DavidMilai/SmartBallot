1c7fba0ff34cb67d7dd9f520cec0b29268bd4ef1
DavidMilai <davidmilai67@gmail.com>
2022-06-08 12:32:54 +0300

    set state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 import Web3 from 'web3';
 
+
 function App() {
+
+  const [name, setName] = useState('myName');
+
   return (
     <div className="App">
       <header className="App-header">
-        Smart Ballot
+        Smart Ballot ({name})
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </header>
     </div>
   );
