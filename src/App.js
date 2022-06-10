cec4a7a5cb1d8b000b72ae525a55d7b3b20ac4fc
DavidMilai <davidmilai67@gmail.com>
2022-06-10 14:46:57 +0300

    set state

--- src/App.js
+++ src/App.js
@@ -1,51 +1,36 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import Web3 from 'web3';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 function App() {
-
   const [account, setAccount] = useState();
   const [balance, setBalance] = useState();
   const [network, setNetwork] = useState();
-  const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
-
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 
   useEffect(() => {
-    async function loadAccounts(){
+    async function loadAccounts() {
       const accounts = await web3.eth.requestAccounts();
       setAccount(accounts[0]);
     }
 
-async function loadBalance(){
-  const network = await web3.eth.getNetworkType();
-  const balance = await web3.eth.getBalance(account);
+    async function loadBalance() {
+      const network = await web3.eth.getNetworkType();
+      const balance = await web3.eth.getBalance(account);
 
-  setBalance(balance);
-  setNetwork(network);  
-
-}
-
-      loadAccounts();
-      loadBalance();
-
-  },[]);
-
+      setBalance(balance);
+      setNetwork(network);
+    }
+    loadAccounts();
+    loadBalance();
+  }, [account]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        Smart Ballot 
-      </header>
-      <p>
-        your account: {account}
-      </p>
-      <p>
-        your network: {network}
-      </p>
-      <p>
-        your balance: {balance}
-      </p>
+      <header className="App-header">Smart Ballot</header>
+      <p>your account: {account}</p>
+      <p>your network: {network}</p>
+      <p>your balance: {balance}</p>
     </div>
   );
 }
