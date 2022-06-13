294431153c38cd1f5ab25d5db9ed758addb9a80f
DavidMilai <davidmilai67@gmail.com>
2022-06-13 16:57:28 +0300

    clean app

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-import Web3 from "web3";
+import { DAppProvider } from "@usedapp/core";
+
 import Wallet from "./components/wallet";
 
 function App() {
-  const [account, setAccount] = useState();
-  const [balance, setBalance] = useState();
-  const [network, setNetwork] = useState();
-  const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-
-  useEffect(() => {
-    loadAccounts();
-  });
-
-  useEffect(() => {
-    loadBalance();
-  }, [account]);
-
-  async function loadAccounts() {
-    const accounts = await web3.eth.requestAccounts();
-    setAccount(accounts[0]);
-  }
-
-  async function loadBalance() {
-    const network = await web3.eth.getNetworkType();
-    const balance = await web3.eth.getBalance(account);
-
-    setBalance((balance/1e18).toFixed(4));
-    setNetwork(network);
-  };
-
   return (
-    <div >
-      <header>Smart Ballot</header>
-      <Wallet/>
-      <p>your account: {account}</p>
-      <p>your network: {network}</p>
-      <p>your balance: {balance}</p>
-    </div>
+    <DAppProvider>
+      <div className="App">
+        <header className="App-header">
+          <Wallet />
+        </header>
+      </div>
+    </DAppProvider>
   );
 }
 
